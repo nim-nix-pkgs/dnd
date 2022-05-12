577db2d62ddcb1399edb0fe7efc967d064d91986
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:05 -0300

    chore: re index dnd

--- flake.nix
+++ flake.nix
@@ -7,6 +7,38 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."dnd-master".dir   = "master";
+  inputs."dnd-master".owner = "nim-nix-pkgs";
+  inputs."dnd-master".ref   = "master";
+  inputs."dnd-master".repo  = "dnd";
+  inputs."dnd-master".type  = "github";
+  inputs."dnd-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."dnd-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."dnd-v0_5_0".dir   = "v0_5_0";
+  inputs."dnd-v0_5_0".owner = "nim-nix-pkgs";
+  inputs."dnd-v0_5_0".ref   = "master";
+  inputs."dnd-v0_5_0".repo  = "dnd";
+  inputs."dnd-v0_5_0".type  = "github";
+  inputs."dnd-v0_5_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."dnd-v0_5_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."dnd-v0_6_0".dir   = "v0_6_0";
+  inputs."dnd-v0_6_0".owner = "nim-nix-pkgs";
+  inputs."dnd-v0_6_0".ref   = "master";
+  inputs."dnd-v0_6_0".repo  = "dnd";
+  inputs."dnd-v0_6_0".type  = "github";
+  inputs."dnd-v0_6_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."dnd-v0_6_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."dnd-v0_6_1".dir   = "v0_6_1";
+  inputs."dnd-v0_6_1".owner = "nim-nix-pkgs";
+  inputs."dnd-v0_6_1".ref   = "master";
+  inputs."dnd-v0_6_1".repo  = "dnd";
+  inputs."dnd-v0_6_1".type  = "github";
+  inputs."dnd-v0_6_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."dnd-v0_6_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
